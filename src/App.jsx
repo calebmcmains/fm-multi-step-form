ecab84e12308867874adc48733050fc82249d94f
calebmcmains <calebmcmains@gmail.com>
2023-10-07 21:02:49 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className=" text-8xl font-bold">Multi-step Form</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line no-undef
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Ubuntu", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
