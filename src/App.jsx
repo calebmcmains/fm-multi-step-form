8ab207e7b04b4f8ea2796ab08c30fdc525512d14
calebmcmains <calebmcmains@gmail.com>
2023-10-07 21:07:55 -0700

    first dev commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className=" text-8xl font-bold">Multi-step Form</h1>
+        <h1 className=" text-8xl font-bold">Multi-step Form!!</h1>
       </div>
     </>
   );
